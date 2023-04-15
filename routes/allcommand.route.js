8c561e942902de22c6193f0a60c154fa03a3b07d
johnkennady220 <115686295+johnkennady220@users.noreply.github.com>
2023-04-15 17:48:51 +0530

    first commit

---
+++ routes/allcommand.route.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { getingcommand } from '../services/allcommand.service.js';
+
+
+
+const router = express.Router();
+
+router.get('/command/:id', async function(request,response){
+    const {id} = request.params
+      
+    const getingCommand = await getingcommand(id)
+  
+    
+  response.send(getingCommand)
+  })
+  export default router
+
+
